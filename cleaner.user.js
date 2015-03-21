a60b318dfaa4571a4bcc593772aea10693b06fa7
Kiny <kiny@KinydeMac-Pro.local>
2015-03-21 08:09:07 +0800

    update

--- cleaner.user.js
+++ cleaner.user.js
@@ -5,25 +5,28 @@
 // @include     http://www.justinyan.me/post/*
 // @include     http://onevcat.com/*
 // @include     http://blog.devtang.com/blog/
+// @include     http://hikiny.com
 // @version     1
 // @grant       none
 // @require     https://code.jquery.com/jquery-1.7.2.min.js
 // @downloadURL https://raw.githubusercontent.com/webpatch/Web-Page-Cleaner/master/cleaner.user.js
 // @updateURL   https://raw.githubusercontent.com/webpatch/Web-Page-Cleaner/master/cleaner.meta.js
 // ==/UserScript==
+
+
 this.$ = this.jQuery = jQuery.noConflict(true);
 
 function insertCSS (cssText) {
   var a = document.createElement("link");
   a.rel = "stylesheet";
-  a.href = "data:text/css,"+encodeURIComponent(cssText)
-  document.getElementsByTagName("head")[0].appendChild(a)
+  a.href = "data:text/css,"+encodeURIComponent(cssText);
+  document.getElementsByTagName("head")[0].appendChild(a);
 }
 
 function cleanTags(tags) {
   for each (i in tags)
   {
-    $(i).remove()
+    $(i).remove();
   }
 }
 
@@ -42,39 +45,12 @@ function cleanTagsExclude(e){
   cleanTagsExclude(tag.parent());
 }
 
+$.get("https://raw.githubusercontent.com/webpatch/Web-Page-Cleaner/master/rules.txt",function(data){
+  var rules = JSON.parse(data)
+  console.log(rules)
+})
 
-var rules = [
-  {
-    "site":"http://www.cocoachina.com/*",
-    "hold":".detail-main",
-    "delete":["#comiframe",".part-wrap",".wx_article",".crumbs",".tgbox",".article-prev",".article-next","#zanbox"],
-    "css":".middle,.detail-left{width: auto !important;}.float-l{float: inherit}.info-detail, .detail-left{padding: 0;}.detail-left .detail-main{background-color:#fff;border: none}"
-  },
-  {
-    "site":"http://www.justinyan.me/post/*",
-    "hold":"article",
-    "delete":["footer",".section_separator",".wp-posturl"],
-    "css":"#content{margin: 0 1em;width: auto;} body{border-top:0;}"
-  },
-  {
-    "site":"http://onevcat.com/*",
-    "hold":"article",
-    "delete":[".post-comments",".post-meta"],
-    "css":".content-wrapper{margin-left: 0;width: auto;}.content-wrapper__inner{margin:1em;padding:0;}"
-  },
-  {
-    "site":"http://blog.devtang.com/blog/*",
-    "hold":"article.hentry",
-    "delete":["footer"],
-    "css":"#content{margin:0}body{max-width:inherit}body > header, body > nav, body > footer, body #content > article, body #content > div > article, body #content > div > section{padding:2em}"
-  },
-  {
-    "site":"^http://www\.raywenderlich\.com/\d+/\S+",
-    "hold":"article",
-    "delete":[".social-links"],
-    "css":"article{border:0;}html{background:#fff}#main{padding:0}.wrapper{max-width:inherit;width:auto;}article:hover, .rw-module:hover{box-shadow:none;}#primary-container,#primary{margin:0;float:none;}"
-  }
-]
+
 
 var href = window.location.href;
 for each (r in rules)

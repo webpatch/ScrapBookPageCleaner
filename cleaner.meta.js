214b63b30d5d2015fba3e0c9d48e307afe544b61
Kiny <kiny@KinydeMac-Pro.local>
2015-03-29 00:09:11 +0800

    add script

--- cleaner.meta.js
+++
@@ -1,7 +0,0 @@
-// ==UserScript==
-// @name        Web page cleaner
-// @namespace   webpatch@sina.com
-// @version     1
-// @downloadURL https://raw.githubusercontent.com/webpatch/Web-Page-Cleaner/master/cleaner.user.js
-// @updateURL   https://raw.githubusercontent.com/webpatch/Web-Page-Cleaner/master/cleaner.meta.js
-// ==/UserScript== 
---
+++ data/handler.js
@@ -0,0 +1,52 @@
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+function insertCSS (cssText) {
+  var a = document.createElement("link");
+  a.rel = "stylesheet";
+  a.href = "data:text/css,"+encodeURIComponent(cssText);
+  document.getElementsByTagName("head")[0].appendChild(a);
+}
+
+function cleanTags(tags) {
+  for each (i in tags)
+  {
+    $(i).remove();
+  }
+}
+
+function cleanTagsExclude(e){
+  if(e == undefined) return ;
+  var tag = $(e);
+  if (tag[0].tagName == "BODY") return; 
+  tag.siblings().each(
+    function(){
+      var name = this.tagName;
+      if (name != "SCRIPT" && name != "LINK")
+      {
+        $(this).remove();
+      }
+    }
+  )
+  cleanTagsExclude(tag.parent());
+}
+
+self.port.on("drawBorder", function(rules) {
+	applyRules(rules);
+});
+
+function applyRules(rules)
+{
+  // console.log(rules);
+  var href = window.location.href;
+  for each (r in rules)
+  {
+    var reg = new RegExp(r["site"])
+    if (reg.test(href) !== false)
+    {
+      cleanTagsExclude(r["hold"])
+      insertCSS(r["css"])
+      cleanTags(r["delete"])
+      break;
+    }
+  }
+}
